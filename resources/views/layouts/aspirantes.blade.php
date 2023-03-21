2f307c59cddb0ca2b1a4112c04038fd65756d24c
DavidCutz <davidcutz25cona@gmail.com>
2023-03-21 17:58:47 -0600

    aspirantes

--- resources/views/layouts/aspirantes.blade.php
+++ resources/views/layouts/aspirantes.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <title>@yield('title') | {{ config('app.name') }}</title>
+    <title>@yield('title') UTC</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Ionicons -->
@@ -22,6 +22,7 @@
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('web/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('web/css/components.css')}}">
+    <link rel="icon" type="image/png" href="img/utc.png">
 
 @yield('page_css')
 
