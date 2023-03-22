e241a86b13eb9da20b6644145e9f57ecfb205951
DavidCutz <davidcutz25cona@gmail.com>
2023-03-22 12:49:17 -0600

    vista aspirantes

--- resources/views/layouts/aspirantes.blade.php
+++ resources/views/layouts/aspirantes.blade.php
@@ -1,91 +1,67 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <title>@yield('title') UTC</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Ionicons -->
-    <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-<!-- EXPORTAR EXEL -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
-<!-- SELECT 2 -->
- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link rel="" href="https://cdn.datatables.net/fixedheader/3.1.6/css/fixedHeader.dataTables.min.css"> 
-    <link href="//fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <link href="{{ asset('assets/css/@fortawesome/fontawesome-free/css/all.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/css/iziToast.min.css') }}">
-    <link href="{{ asset('assets/css/sweetalert.css') }}" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="https://kit.fontawesome.com/270e7df619.css" crossorigin="anonymous">
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('web/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('web/css/components.css')}}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/aspirantes.css">
     <link rel="icon" type="image/png" href="img/utc.png">
-
-@yield('page_css')
-
-
-  <!-- AQUI EMPIEZA EL HEAD BOSTRAP -->
-  <section class="section ">
-        <div class="section-header ">
-                <div class="col-lg-12 ">
-                        <div class="card-body alert-warning">                          
-                                <div class="row a">
-                                    <div ></div> 
-                                    <div >
-                                    
-  <div class="container-fluid">
-    <h2 class=" text-center text-warning"><img class="rounded float-start" alt="image" src="{{ asset('img/LogoOf.png') }}" width="250" height="120">BIENVENIDO ASPIRANTE A LA UNIVERSIDAD TECNOLOGICA DEL CENTRO  </h2>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    </div>
-  </div>
-
-</div>                        
-                        </div>
-                    </div>
-                </div>
+    <title>UTC</title>
+</head>
+<body>
+    <div class="container">
+        <div class="title">Bienvenido a la UTC
+            <img class="logo" src="img/utc.png" alt="">
+        </div>
+        <form action="#">
+            <div class="form-group">
+                <span>Folio</span>
+                <input type="text" placeholder="Folio" required>
             </div>
-
-</section>
-
-      @yield('content')
-
-  
-
-
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/popper.min.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/sweetalert.min.js') }}"></script>
-<script src="{{ asset('assets/js/iziToast.min.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.nicescroll.js') }}"></script>
-<script src="https://kit.fontawesome.com/270e7df619.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-            <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-            <!-- Para usar los botones -->
-            <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-            <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-            <!-- Para los estilos en Excel -->
-            <script src="https://cdn.jsdelivr.net/npm/datatables-buttons-excel-styles@1.1.1/js/buttons.html5.styles.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/datatables-buttons-excel-styles@1.1.1/js/buttons.html5.styles.templates.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
- <script src="https://cdn.datatables.net/fixedheader/3.1.6/js/dataTables.fixedHeader.min.js"></script>  
-<!-- Template JS File -->
-<script src="{{ asset('web/js/stisla.js') }}"></script>
-<script src="{{ asset('web/js/scripts.js') }}"></script>
-<script src="{{ mix('assets/js/profile.js') }}"></script>
-<script src="{{ mix('assets/js/custom/custom.js') }}"></script>
-@yield('page_js')
-@yield('scripts')
-@yield('js')
- 
-  </body>
+            <div class="form-group">
+                <span>Nombre Completos</span>
+                <input type="text" placeholder="Nombres" required>
+            </div>
+            <div class="form-group">
+                <span>Apellido Paterno</span>
+                <input type="email" placeholder="Apellidos Paternos" required>
+            </div>
+            <div class="form-group">
+                <span>Apellido Materno</span>
+                <input type="text" placeholder="Apellidos Maternos" required>
+            </div>
+            <div class="form-group">
+                <span>Curp</span>
+                <input type="text" placeholder="Curp" required>
+            </div>
+            <div class="form-group">
+                <span>Correo</span>
+                <input type="text" placeholder="Correo" required>
+            </div>
+            <div class="form-group">
+                <span>Telefono</span>
+                <input type="text" placeholder="Telefono" required>
+            </div>
+            <div class="form-group">
+                <span>Localidad</span>
+                <input type="text" placeholder="Localidad" required>
+            </div>
+            <div class="form-group">
+                <span>Genero</span>
+                <input type="text" placeholder="Genero" required>
+            </div>
+            <div class="form-group">
+                <span>Procedencia</span>
+                <input type="text" placeholder="Procedencia" required>
+            </div>
+            <div class="form-group">
+                <span>Carrera</span>
+                <input type="text" placeholder="Carrera" required>
+            </div>
+            <div class="button">
+                <input type="button" value=" Regristar">
+            </div>
+        </form>
+    </div>
+</body>
 </html>
