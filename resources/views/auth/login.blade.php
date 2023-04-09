4aff84df7ae47cf3bd03605bdf517413fe1b8566
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-04-08 19:21:53 -0500

    push login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.auth_app')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
-<link rel="stylesheet" href="css/login1.css">
+<link rel="stylesheet" href="all-css/login.css">
 @section('title')
 Admin Login
 @endsection
