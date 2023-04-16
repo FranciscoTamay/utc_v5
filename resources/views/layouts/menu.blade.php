127cb38fad874a0a9897f944abf2144d95b4e762
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-04-16 15:23:18 -0500

    cosas

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,10 @@
 <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
     <a class="nav-link" href="/home">
-        <i class=" fas fa-building" title="Inicio"></i><span>Inico</span>
+        <i class=" fas fa-building"></i><span>Inico</span>
     </a>
 
     <a class="nav-link" href="/usuarios">
-        <i class=" fas fa-users" title="Usuarios"></i><span>Usuarios</span>
+        <i class=" fas fa-users"></i><span>Usuarios</span>
     </a>
 
     <!-- <a class="nav-link" href="/roles">
@@ -16,60 +16,60 @@
     </a> -->
 
     <a class="nav-link" href="/carreras">
-        <i class="fas fa-graduation-cap" title="Carreras"></i><span>Carreras</span>
+        <i class="fas fa-graduation-cap" ></i><span>Carreras</span>
     </a>
 
     <a class="nav-link" href="/planEstudio">
-        <i class="fas fa-clipboard" title="Plan Estudio"></i><span>Plan Estudio</span>
+        <i class="fas fa-clipboard"></i><span>Plan Estudio</span>
     </a>
 
 
     <a class="nav-link" href="/asignaturas">
-        <i class="fas fa-book" title="Asignaturas"></i><span>Asignaturas</span>
+        <i class="fas fa-book" ></i><span>Asignaturas</span>
     </a>
 
     <a class="nav-link" href="/alumnos">
-        <i class="fas fa-user-graduate" title="Alumnos"></i><span>Alumnos</span>
+        <i class="fas fa-user-graduate" ></i><span>Alumnos</span>
     </a>
 
     <a class="nav-link" href="/servicios">
-        <i class="fas fa-staff-snake" title="Servicios"></i><span>Servicios</span>
+        <i class="fas fa-staff-snake" ></i><span>Servicios</span>
     </a>
 
     <a class="nav-link" href="/registrop">
-        <i class="fas fa-money-check-dollar" title="Registro de Pagos"></i><span>Registro Pagos</span>
+        <i class="fas fa-money-check-dollar" ></i><span>Pagos</span>
     </a>
 
     <a class="nav-link" href="/planEstudio">
-        <i class="fas fa-book" title="Plan de Estudio"></i><span>Plande Estudio</span>
+        <i class="fas fa-book" ></i><span>Plan Estudio</span>
     </a>
 
     <a class="nav-link" href="/asp">
-        <i class="fas fa-clipboard-question" title="Aspirantes"></i><span>Aspirantes</span>
+        <i class="fas fa-clipboard-question" ></i><span>Aspirantes</span>
     </a>
 
     <a class="nav-link" href="/matriculas">
-        <i class="fas fa-id-card-clip" title="Matriculas"></i><span>Matriculas</span>
+        <i class="fas fa-id-card-clip" ></i><span>Matriculas</span>
     </a>
 
     <a class="nav-link" href="/grupos">
-        <i class="fas fa-people-group" title="Grupos"></i><span>Grupos</span>
+        <i class="fas fa-people-group" ></i><span>Grupos</span>
     </a>
 
     <a class="nav-link" href="/maestros">
-        <i class="fas fa-chalkboard-user" title="Maestros"></i><span>Maestros</span>
+        <i class="fas fa-chalkboard-user" ></i><span>Maestros</span>
     </a>
 
     <a class="nav-link" href="/procedencias">
-        <i class="fas fa-black-tie" title="Procedencias"></i><span>Procedencias</span>
+        <i class="fas fa-black-tie" ></i><span>Procedencias</span>
     </a>
 
     <a class="nav-link" href="/grados">
-        <i class="fas fa-boxes-stacked" title="Grados"></i><span>Grados</span>
+        <i class="fas fa-boxes-stacked"></i><span>Grados</span>
     </a>
 
     <a class="nav-link" href="/calificaciones">
-        <i class="fas fa-envelope-open-text" title="Calificaciones"></i></i><span>Calificaciones</span>
+        <i class="fas fa-envelope-open-text" ></i></i><span>Calificaciones</span>
     </a>
 
 </li>
